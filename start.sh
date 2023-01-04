23cd80fe0eca9a1feb1c5fff5b6df5740d0c788c
Akshay962 <107416405+Akshay962@users.noreply.github.com>
2023-01-04 08:13:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/Akshay962/SD.git /SD
+cd /Akshay962
 pip3 install -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot....🍿"
 python3 main.py
